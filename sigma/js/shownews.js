e9e92ddac1b444de15fef52863d70e1122266738
Haifeng Xu <hx4ad@csroots-MacBook-Air.local>
2021-09-28 21:37:54 -0400

    updates

--- sigma/js/shownews.js
+++ sigma/js/shownews.js
@@ -1,5 +1,6 @@
 var news = new Array();
-news[20] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Aug 2021 </td><td>&nbsp;  Our lab receives an  <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2132506&HistoricalAwards=false">NSF grant on algorithmic persuasion</a>  </td></tr>';
+news[21] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Sep 2021 </td><td>&nbsp; Three papers accepted to <a href="https://nips.cc/Conferences/2021">NeurIPS</a>; congrats to Minbiao and Jibang for their first publication in our group </td></tr>';
+news[20] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Aug 2021 </td><td>&nbsp;  Our lab received an  <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2132506&HistoricalAwards=false">NSF grant on algorithmic information design</a>  </td></tr>';
 news[19] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; July 2021 </td><td>&nbsp;  Talk on mechanism design for selling information at  <a href="http://www.gtcenter.org/?page=Workshop_WSCL.html">Strategic Communication and Learning Workshop</a>  </td></tr>';
 news[18] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; July 2021 </td><td>&nbsp;  Tutorial on learning and game theory at <a href="https://www.cs.cityu.edu.hk/~gtsc/key-and-t.html#/">Summer School on Game Theory</a> </td></tr>';
 news[17] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; April 2021 </td><td>&nbsp;  We have three <a href="https://ec21.sigecom.org/">EC</a> and two <a href="https://icml.cc/">ICML</a> papers accepted. Congrats to Shuze, Quinn and Fan for their first publication </td></tr>';
