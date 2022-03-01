78a7d312bf8cda262872efb8b86752736833f462
Haifeng Xu <hx4ad@csroots-MacBook-Air.local>
2022-03-01 11:20:29 -0500

    typos

--- sigma/js/shownews.js
+++ sigma/js/shownews.js
@@ -1,5 +1,5 @@
 var news = new Array();
-news[23] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Feb 2022 </td><td>&nbsp; I am serving as a PC co-chair for <a href="https://gamesec-conf.org/index.php">GameSec 2022: Game Theory for Security</a> conference; Please consider submit!</td></tr>';
+news[23] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Feb 2022 </td><td>&nbsp; I am a technical program co-chair for <a href="https://gamesec-conf.org/index.php">GameSec 2022: Game Theory for Security</a> conference; Please consider submiting!</td></tr>';
 news[22] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Feb 2022 </td><td>&nbsp; Five papers accepted to <a href="https://aaai.org/Conferences/AAAI-22/">AAAI 2022</a> (acceptance rate 15%), with three oral presentations </td></tr>';
 news[21] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Sep 2021 </td><td>&nbsp; Three papers accepted to <a href="https://nips.cc/Conferences/2021">NeurIPS 2021</a>; congrats to Minbiao and Jibang for their first publication in our group </td></tr>';
 news[20] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Aug 2021 </td><td>&nbsp;  Grateful to NSF for supporting <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2132506&HistoricalAwards=false">our project on algorithmic information design</a>  </td></tr>';
